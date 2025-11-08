e3ace808e2657fbc8feecde0a68d35ed7eca9069
Chetan_K <chetan7766ar@gmail.com>
2025-11-08 10:28:33 +0530

    Organizing Folders

---
+++ main/script.js
@@ -0,0 +1,83 @@
+const addBtn = document.querySelector('#addbtn');
+const main = document.querySelector('.container')
+const remove = document.querySelector('.remove-Note');
+
+// colling add note function
+addBtn.addEventListener(
+    "click",
+    function () {
+        addNote()
+    }
+);
+
+// function to save the Notes
+const saveNots = () => {
+    const Notes = document.querySelectorAll(".note textarea");
+    console.log(Notes);
+
+    const Data = [];
+    Notes.forEach((note) => {
+        Data.push(note.value);
+    });
+    console.log(Data);
+
+    localStorage.setItem("Notes", JSON.stringify(Data));
+}
+
+
+// function to add note
+const addNote = (text = "") => {
+    const note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = `
+            <div class="note-navbar">
+                <ul>
+                    <li class="edit" >
+                        Edit
+                    </li>
+                    <li class="save">
+                        Save
+                    </li>
+                    <li class="remove-Note">
+                    Delete
+                    </li>
+                </ul>
+            </div>
+            <textarea>${text}</textarea>
+        `;
+
+    //function to remove note
+    note.querySelector(".remove-Note").addEventListener("click", function () {
+        note.remove();
+        saveNots();
+    }
+    );
+
+    // function to saveMnually
+    note.querySelector(".save").addEventListener("click", function () {
+        saveNots();
+    });
+
+    // function use for autoSave
+    note.querySelector("textarea").addEventListener(
+        "focusout",
+        function () {
+            saveNots()
+        }
+    )
+
+    main.appendChild(note);
+    saveNots()
+}
+// self colling function to put and get data from the localStorage
+(function () {
+    const lsNotes = JSON.parse(localStorage.getItem("Notes"));
+    console.log(lsNotes);
+    lsNotes.forEach(
+        (lsNotes) => {
+            addNote(lsNotes)
+        }
+    )
+
+}
+)()
